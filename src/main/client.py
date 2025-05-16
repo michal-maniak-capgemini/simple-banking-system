02b831aaa8352f91300b225c1175ea2ab06799d9
michal-maniak-capgemini <michal.maniak@capgemini.com>
2025-05-16 08:37:46 +0200

    Refactor client class - add private methods, add unique id, refactor balance and amount to float

--- src/main/client.py
+++ src/main/client.py
@@ -1,38 +1,50 @@
+import uuid
+from typing import List
+
 from .transaction import Transaction
 from .transaction_type import TransactionType
 from datetime import datetime
 
 class Client:
-    def __init__(self, name: str, balance: int):
-        if balance < 0:
-            raise ValueError("Balance can't be negative")
-        self.name = name
-        self.balance = balance
-        self.__transactions = []
+    def __init__(self, name: str, balance: float = 0):
+        self.__check_if_negative(balance)
+        self.client_id: uuid = uuid.uuid4()
+        self.name: str = name
+        self.__balance: float  = balance
+        self.__transactions: List[Transaction] = []
 
     def __repr__(self):
-        return f"Client(name={self.name}, balance={self.balance})"
+        return f"Client(client_id={repr(self.client_id)}, name={repr(self.name)}, balance={repr(self.__balance)}, transactions={repr(self.__transactions)})"
 
-    def deposit(self, amount: int):
-        if amount < 0:
-            raise ValueError("Can't deposit negative amount")
-        self.balance += amount
+    def deposit(self, amount: float) -> None:
+        self.__check_if_negative(amount)
+        self.__balance += amount
         transaction = Transaction(self.name, TransactionType.DEPOSIT, amount, datetime.now())
         self.__transactions.append(transaction)
 
-    def withdraw(self, amount: int):
-        if amount < 0:
-            raise ValueError("Can't withdraw negative amount")
-        elif amount > self.balance:
-            raise ValueError("Can't withdraw more than balance")
-        self.balance -= amount
+    def withdraw(self, amount: float) -> None:
+        self.__check_if_negative(amount)
+        self.__check_if_more_than_balance(amount)
+        self.__balance -= amount
         transaction = Transaction(self.name, TransactionType.WITHDRAWAL, amount, datetime.now())
         self.__transactions.append(transaction)
 
-    def get_balance(self) -> int:
-        return self.balance
+    @property
+    def get_balance(self) -> float:
+        return self.__balance
 
-    def print_statement(self):
+    def print_statement(self) -> None:
         print("Client transaction history:")
+        if not self.__transactions:
+            print(f"{datetime.now()} No transactions so far.")
         for transaction in self.__transactions:
             print(transaction)
+
+    @staticmethod
+    def __check_if_negative(value: float) -> None:
+        if value < 0:
+            raise ValueError("Balance/Amount can't be negative")
+
+    def __check_if_more_than_balance(self, amount) -> None:
+        if amount > self.__balance:
+            raise ValueError("Can't withdraw more than balance")
--- src/test/test_client.py
+++ src/test/test_client.py
@@ -1,42 +1,44 @@
+import math
+
 import pytest
 
 from src.main.client import Client
 from datetime import datetime
 
 def test_balance_below_0():
-    with pytest.raises(ValueError, match="Balance can't be negative"):
+    with pytest.raises(ValueError, match="Balance/Amount can't be negative"):
         Client("John", -1)
 
 def test_get_balance():
-    client = Client("John", 1000)
-    assert client.get_balance() == 1000
+    client = Client("John", 1000.54)
+    assert math.isclose(client.get_balance, 1000.54, rel_tol=1e-09, abs_tol=1e-09)
 
 def test_deposit_negative_amount():
     client = Client("John", 1000)
-    with pytest.raises(ValueError, match="Can't deposit negative amount"):
+    with pytest.raises(ValueError, match="Balance/Amount can't be negative"):
         client.deposit(-1)
 
 def test_deposit():
     client = Client("John", 1000)
     client.deposit(500)
-    assert client.get_balance() == 1500
+    assert client.get_balance == 1500
     client.deposit(1000)
-    assert client.get_balance() == 2500
+    assert client.get_balance == 2500
 
 def test_withdraw_negative_amount():
     client = Client("John", 1000)
-    with pytest.raises(ValueError, match="Can't withdraw negative amount"):
+    with pytest.raises(ValueError, match="Balance/Amount can't be negative"):
         client.withdraw(-1)
 
 def test_withdraw_more_than_balance():
     client = Client("John", 1000)
     with pytest.raises(ValueError, match="Can't withdraw more than balance"):
-        client.withdraw(1001)
+        client.withdraw(1001.50)
 
 def test_withdraw():
     client = Client("John", 1000)
     client.withdraw(500)
-    assert client.get_balance() == 500
+    assert client.get_balance == 500
 
 def test_print_statement(mocker, capsys):
     fixed_date = datetime(2021, 7, 1)
@@ -53,5 +55,20 @@ def test_print_statement(mocker, capsys):
     "Client transaction history:\n"
     "Transaction: TransactionType.DEPOSIT of 500 on 2021-07-01 00:00:00 for John\n"
     "Transaction: TransactionType.WITHDRAWAL of 200 on 2021-07-01 00:00:00 for John\n"
+    )
+    assert captured.out == expected_output
+
+def test_print_statement_when_empty_transactions(mocker, capsys):
+    fixed_date = datetime(2021, 7, 1)
+    mock_datetime = mocker.patch('src.main.client.datetime')
+    mock_datetime.now.return_value = fixed_date
+
+    client = Client("John", 1000)
+    client.print_statement()
+    captured = capsys.readouterr()
+
+    expected_output = (
+        "Client transaction history:\n"
+        "2021-07-01 00:00:00 No transactions so far.\n"
     )
     assert captured.out == expected_output
